0c1d520b0f98f5208ebf0bd9b4c22d494d0f31a3
Waleed Lugod <lugodwaleed@gmail.com>
2023-11-17 18:30:07 +0800

    aaaa

--- js/index.js
+++ js/index.js
@@ -1,3 +1,8 @@
 // Insert your JS code here
 
 console.log("Log entry.");
+
+function scaryFunction() {
+	document.getElementById("scary").style.opacity = 1
+	console.log(document.getElementById("scary"))
+}
